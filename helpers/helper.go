d9dfc61700a62761ac28698a0720e15f6fd6981f
Sherlokmo <redmohammad22@gmail.com>
2022-04-17 12:40:44 +0200

    feat(helpers): add provision url functionality

--- helpers/helper.go
+++ helpers/helper.go
@@ -1,7 +1,23 @@
 package helpers
 
+import "errors"
+
 func HnadleError(err error) {
 	if err != nil {
 		panic(err)
 	}
+}
+
+func ProvisionURL(toProvision, base string) (string, error) {
+	var err error = nil
+
+	if len(toProvision) < 1 || toProvision[0] == '#' {
+		err = errors.New("Empty Hyperlink")
+	} else if toProvision[0] == '/' {
+		toProvision = base + toProvision
+	} else if toProvision[0:2] == "./" {
+		toProvision = base + toProvision[1:]
+	}
+
+	return toProvision, err
 }
