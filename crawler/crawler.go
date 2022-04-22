41b852ab8f983f1d897a7af00c8320425e63f7b9
Sherlokmo <redmohammad22@gmail.com>
2022-04-22 17:10:21 +0200

    Implement: Crawl function implement

--- crawler/crawler.go
+++ crawler/crawler.go
@@ -1,23 +1,24 @@
 package crawler
 
 import (
+	"fmt"
 	"goSpider/helpers"
+	"goSpider/link"
+	"goSpider/tokenizer"
 	"io"
 	"net/http"
 )
 
 type spider struct {
 	baseurl string
-	links   []Link
+	links   []link.Link
 	depth   int
 }
-
-const MAX_DEPTH int = 5
 
 func NewSpider(baseurl string, depth int) *spider {
 	return &spider{
 		baseurl: baseurl,
-		links:   make([]Link, 0),
+		links:   make([]link.Link, 0),
 		depth:   depth,
 	}
 }
@@ -25,6 +26,11 @@ func NewSpider(baseurl string, depth int) *spider {
 func (s *spider) Crawl() {
 	baseBody := s.callBase()
 	defer baseBody.Close()
+	spiderLegs := tokenizer.NewTokenizer(baseBody, s.baseurl)
+	Links := spiderLegs.SplitAnchors()
+	for _, Link := range *Links {
+		fmt.Println(Link.Url)
+	}
 }
 
 func (s *spider) callBase() io.ReadCloser {
--- crawler/link.go
+++
@@ -1,6 +0,0 @@
-package crawler
-
-type Link struct {
-	Title, Url string
-	Links      *[]Link
-}
