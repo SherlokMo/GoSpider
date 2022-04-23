4f41591043ca795f65a9841a48c5ad3ced106025
Sherlokmo <redmohammad22@gmail.com>
2022-04-23 16:19:33 +0200

    refactor: change property name

--- memorydb/memorydb.go
+++ memorydb/memorydb.go
@@ -3,23 +3,23 @@ package memorydb
 import "sync"
 
 type MemDB[K Ikey, V any] struct {
-	visited map[K]V
+	hashmap map[K]V
 	mu      sync.RWMutex
 }
 
 func NewMemorydb[K Ikey, V any]() *MemDB[K, V] {
 	return &MemDB[K, V]{
-		visited: make(map[K]V),
+		hashmap: make(map[K]V),
 	}
 }
 
 func (m *MemDB[K, V]) Store(key K, v V) {
 	m.mu.Lock()
 	defer m.mu.Unlock()
-	m.visited[key] = v
+	m.hashmap[key] = v
 }
 
 func (m *MemDB[K, T]) Exist(key K) bool {
-	_, ok := m.visited[key]
+	_, ok := m.hashmap[key]
 	return ok
 }
