e84c1be7a5ab16343d9b87227a1c861f8e902134
Sherlokmo <redmohammad22@gmail.com>
2022-04-26 00:07:39 +0200

    test: add unit test to cralwer struct

---
+++ crawler/crawler_test.go
@@ -0,0 +1,30 @@
+package crawler
+
+import (
+	"bytes"
+	"testing"
+)
+
+func TestVisited(t *testing.T) {
+	testSite := "https://example.com"
+	spider := NewSpider(1)
+	spider.addToVisited(testSite)
+	if !spider.isVisited(testSite) {
+		t.Error("Expected visited recieved false")
+	}
+}
+
+func TestWebMethod(t *testing.T) {
+	testSite := "https://google.com"
+	stream := []byte(`
+			<a href="https://example.com">E1</a>
+			<a href="https://facebook.com">E2</a> 
+			<a href="https://gmail.com">E3</a>
+		`)
+	reader := bytes.NewReader(stream)
+	spider := NewSpider(1)
+	Links := spider.web(reader, testSite)
+	if len(*Links) != 3 {
+		t.Errorf("Expected 2 sites recieved %v", len(*Links))
+	}
+}
